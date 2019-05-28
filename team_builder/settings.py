fa303f53f96c82bb8bbe0231b3642a0aeba6589e
Alexander Mueller <1992alex.m@googlemail.com>
2019-05-28 17:15:30 +0200

    Changed media storage to AWS S3

--- team_builder/settings.py
+++ team_builder/settings.py
@@ -47,6 +47,8 @@ INSTALLED_APPS = [
     'projects',
     'braces',
     'notifications',
+
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -151,3 +153,16 @@ EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_EMAIL_VERIFICATION = True
 ACCOUNT_LOGIN_ON_EMAIL_CONFIRMATION = True
+
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+# Sets the Signature Version to V4 + the correct region
+AWS_S3_REGION_NAME = "eu-central-1"
+AWS_S3_SIGNATURE_VERSION = "s3v4"
